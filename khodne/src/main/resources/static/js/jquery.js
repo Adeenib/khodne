38e6bf7fdba6a652298cefdfca7277c3f50a7781
asmaalasaada <asma.alsaada96@gmail.com>
2021-07-04 17:23:06 +0300

    asma push

--- khodne/src/main/resources/static/js/jquery.js
+++ khodne/src/main/resources/static/js/jquery.js
@@ -1,7 +1,1 @@
 /*! jQuery v3.5.1 | (c) JS Foundation and other contributors | jquery.org/license */
-
-$(document).ready(function(){
-  $("button").click(function(){
-    $("p").toggle();
-  });
-});s
